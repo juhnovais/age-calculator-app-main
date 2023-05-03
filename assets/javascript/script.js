ff8bf38e3df991776ef69482ecab4272c1ee0773
juhfalleirosnovais <60365178+juhfalleirosnovais@users.noreply.github.com>
2023-05-03 22:26:28 +0100

    js code updated

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function () {
   var inputYear = $(".inputYear");
   var inputMonth = $(".inputMonth");
@@ -6,15 +5,13 @@ $(document).ready(function () {
   var formInputs = $(".formInputs");
   var dateInputs = $("#day, #month, #year");
 
-  
-
-  function addWarningMessage(input, message) {
+  const addWarningMessage = (input, message) => {
     if (!input.has(".pWarning").length) {
       input.append("<p class='pWarning'>" + message + "</p>");
       input.find("label").addClass("labelWarning");
       input.find("input").addClass("inputWarning");
     }
-  }
+  };
   function addWarningInput(input) {
     if (!input.has(".pWarning").length) {
       input.find("label").addClass("labelWarning");
@@ -25,99 +22,77 @@ $(document).ready(function () {
   $("#ageCalculator").submit(function (e) {
     e.preventDefault(); // prevent the form from submitting
 
-    var day = parseInt($("#day").val());
-    var month = parseInt($("#month").val());
-    var year = parseInt($("#year").val());
+    var dayVal = parseInt($("#day").val());
+    var monthVal = parseInt($("#month").val() - 1);
+    var yearVal = parseInt($("#year").val());
 
-    var today = new Date();
+    const isValidDate = (year, month, day) => {
+      var d = new Date(year, month, day);
+      var td = new Date();
 
-    // get the current year, month and day
-    var currentYear = today.getFullYear();
-    var currentMonth = today.getMonth();
-    var currentDay = today.getDate();
+      var diff = td - new Date(year, month, day);
 
-    // create a new Date object with the user input (using 0 for the day value)
-    var date = new Date(year, month - 1, 0);
-
-    // get the last day of the month from the Date object
-    var lastDayOfMonth = date.getDate();
-
-    var diff = today - new Date(year, month - 1, day);
-
-
-    // Check year input
-    if (isNaN(year) && isNaN(month) && isNaN(day)) {
-      addWarningMessage(inputYear, "This field is required");
-      addWarningMessage(inputMonth, "This field is required");
-      addWarningMessage(inputDay, "This field is required");
-      $(".monthsCalc span, .daysCalc span, .yearCalc span").text("--");
-      return;
-    };
-    
-    if (year > currentYear) {
-      addWarningMessage(inputYear, "Must be in the past");
-      addWarningInput(inputMonth);
-      addWarningInput(inputDay);
-      $(".monthsCalc span, .daysCalc span, .yearCalc span").text("--");
-      return;
-    }; 
-    
-      var diff_years = Math.floor(diff / (365.25 * 24 * 60 * 60 * 1000));
-      if (diff_years < 0) {
+      if (isNaN(year) || isNaN(month) || isNaN(day)) {
+        addWarningMessage(inputYear, "This field is required");
+        addWarningMessage(inputMonth, "This field is required");
+        addWarningMessage(inputDay, "This field is required");
         $(".monthsCalc span, .daysCalc span, .yearCalc span").text("--");
-      } else {
-        $(".yearCalc span").text(diff_years);
+        return;
       }
 
-    // Check month input
-    if (month - 1 > currentMonth && year > currentYear) {
-      addWarningMessage(inputMonth, "Most be a valid month");
-      $(".monthsCalc span, .daysCalc span, .yearCalc span").text("--");
-      return;
+      if (
+        d.getFullYear() == year &&
+        year <= td.getFullYear() &&
+        d.getMonth() == month &&
+        d.getDate() == day
+      ) {
+        
+
+        // calculate years, months and days
+        var diff_years = Math.floor(diff / (365.25 * 24 * 60 * 60 * 1000));
+        var diff_months = Math.floor(diff / (30.44 * 24 * 60 * 60 * 1000));
+        var diff_days = Math.floor(diff / (24 * 60 * 60 * 1000));
+        if(diff > 0 ) {
+            $(".yearCalc span").text(diff_years);
+            $(".monthsCalc span").text(diff_months);
+            $(".daysCalc span").text(diff_days); 
+        } else {
+
+          $(".monthsCalc span, .daysCalc span, .yearCalc span").text("--");
+
+        }
+
+        return;
+      } else {
+        
+        
+        if (d.getDate() != day || diff_days < 0) {
+          addWarningMessage(inputDay, "Must be a valid day");
+          $(".monthsCalc span, .daysCalc span, .yearCalc span").text("--");
+        }
+        if (d.getMonth() != month || diff_months < 0) {
+          addWarningMessage(inputMonth, "Most be a valid month");
+          $(".monthsCalc span, .daysCalc span, .yearCalc span").text("--");
+        }
+        if (year > td.getFullYear() || diff_years < 0) {
+          addWarningMessage(inputYear, "Must be in the past");
+          $(".monthsCalc span, .daysCalc span, .yearCalc span").text("--");
+        }
+
+        return;
+      }
     };
 
-      var diff_months = Math.floor(diff / (30.44 * 24 * 60 * 60 * 1000));
-      if (diff_months - 1 < 0 && diff_months - 1 > -1) { 
-        $(".monthsCalc span").text(diff_months);
-      } else if (diff_months - 1 < -1) {
-        addWarningInput(inputDay);
-        addWarningMessage(inputMonth, "Most be a valid month");
-        addWarningInput(inputYear);
-        $(".monthsCalc span").text("--");
-      } else {
-        $(".monthsCalc span").text(diff_months);
-      }
-    
+    isValidDate(yearVal, monthVal, dayVal);
 
-    // Check day input
     
-    if (day < 1 || day > lastDayOfMonth) {
-      addWarningMessage(inputDay, "Must be a valid day");
-      addWarningInput(inputMonth);
-      addWarningInput(inputYear);
-      $(".monthsCalc span, .daysCalc span, .yearCalc span").text("--");
-      return;
-    } 
-    
-    var diff_days = Math.floor(diff / (24 * 60 * 60 * 1000)); // divide by number of milliseconds in a day
-    if(diff_days < 0) {
-      addWarningMessage(inputDay, "Must be a valid day");
-      addWarningInput(inputMonth);
-      addWarningInput(inputYear);
-      $(".monthsCalc span, .daysCalc span, .yearCalc span").text("--");
-    } else {
-      $(".daysCalc span").text(diff_days);
-    }
-
-
   });
 
-    $(formInputs).click(function () {
-      $(formInputs).find(".pWarning").remove();
-      $(formInputs).find("label").removeClass("labelWarning");
-      $(formInputs).find("input").removeClass("inputWarning");
-    });
-
+  $(formInputs).click(function () {
+    $(formInputs).find(".pWarning").remove();
+    $(formInputs).find("label").removeClass("labelWarning");
+    $(formInputs).find("input").removeClass("inputWarning");
+  });
 
   // add focus event listeners to each input
   $(dateInputs).each(function () {
